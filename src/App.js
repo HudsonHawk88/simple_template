49a9887c15883c9af471d63c161c654dec048c17
Tóth Gergő <tothg@zalaszam.local>
2019-07-03 15:50:55 +0200

    feltolt

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {} from 'react-bootstrap';
+import {} from 'bootstrap';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+
+  render(){
+// https://getbootstrap.com/docs/4.3/getting-started/introduction/
+return(
+<div class="container">
+  <div class="row">
+      <div class="col-12" id="navbar">Menu</div>
+  </div>
+  <div class="row">
+      <div class="col" id="baloldal">Oldalsáv</div>
+      <div class="col-8" id="tartalom">Tartalom</div>
+      <div class="col" id="jobboldal">Oldalsáv</div>
+  </div>
+  <div class="row">
+      <div class="col-12" id="footer">Footer</div>
+  </div>
+</div>
+
+)
+
+
+
+  }
 }
-
-export default App;
+  export default App;
